6cc4a625b4e4d5004936100cea9617cb2e27b80f
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-08-15 21:38:11 -0300

    Architecture Node application organizadion

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+
 
 // view engine
 app.set('view engine','ejs');
@@ -22,6 +25,9 @@ connection
     }).catch((error) =>{
         console.log(error);
     })
+
+app.use("/",categoriesController) ;
+app.use("/",articlesController) ;
 
 app.get("/", (req,res)=>{
     res.render("index");
