d6f4e0bc8619839c017359de67e4f24af035c3dd
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-08-20 10:43:35 -0300

    category form

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -5,8 +5,8 @@ router.get("/categories", (req,res)=> {
     res.send("Rota de categorias");
 })
 
-router.get("admin/categories/new", (req,res)=> {
-    res.send("Rota para criar uma nova categoria");
+router.get("/admin/categories/new", (req,res)=> {
+    res.render("admin/categories/new");
 })
 
 module.exports = router;
