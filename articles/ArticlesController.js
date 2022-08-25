63ae41722676549d9e2962927e03a355a5099118
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-08-25 20:42:43 -0300

    article form

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,8 +5,8 @@ router.get("/articles", (req,res)=> {
     res.send("Rota de artigos");
 })
 
-router.get("admin/articles/new", (req,res)=> {
-    res.send("Rota para criar uma novo artigo");
+router.get("/admin/articles/new", (req,res)=> {
+    res.render("admin/articles/new");
 })
 
 module.exports = router;
