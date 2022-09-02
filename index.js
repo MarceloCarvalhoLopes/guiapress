a17a17dce44a5cbd6525dca5a7efe84d221769f6
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-09-02 17:36:34 -0300

    article reading page

--- index.js
+++ index.js
@@ -36,9 +36,30 @@ app.use("/",categoriesController) ;
 app.use("/",articlesController) ;
 
 app.get("/", (req,res)=>{
-    Article.findAll().then(articles =>{
+    Article.findAll({
+        order:[
+            ['id', 'DESC']
+        ]
+    }).then(articles =>{
         res.render("index", {articles : articles});
     });  
+});
+
+app.get("/:slug",(req, res) =>{
+    var slug = req.params.slug;
+    Article.findOne({
+        where:{
+            slug:slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            res.render("article",{article : article});
+        }else{
+            res.redirect("/")
+        }
+    }).catch(err =>{
+        res.redirect("/")
+    })
 });
 
 app.listen(8080, ()=> {
