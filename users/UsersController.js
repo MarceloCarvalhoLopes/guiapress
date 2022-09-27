668afaa74e96cdc0fd849c45063e593ab682907e
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-09-27 08:31:36 -0300

    key of hash

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) => {
     res.send("Listagem de usuários");
@@ -13,6 +14,18 @@ router.get("/admin/users/create", (req, res) => {
 router.post("/users/create",(req, res) => {
     var email = req.body.email;
     var password = req.body.password;
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password,salt);
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect("/");
+    }).catch((err) => {
+        res.redirect("/");
+    });
 
     //res.json({email, password});
 })
